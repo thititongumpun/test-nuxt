8d6d2e24b07106725e905ea44f7ad53d0150ca40
thititongumpun <thiti180536@gmail.com>
2022-10-17 11:06:49 +0700

    test something

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: "~/plugins/pwa-update.js", mode: "client" }],
+  plugins: [
+    { src: "~/plugins/pwa-update.js", mode: "client" },
+    { src: "~/plugins/sw-update.js", mode: "client" },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
